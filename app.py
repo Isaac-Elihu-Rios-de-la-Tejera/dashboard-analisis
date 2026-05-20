7e619146b8ec2c48299db2a5603fa0f75371bce6
MikeSalazar3 <160252216+MikeSalazar3@users.noreply.github.com>
2026-05-20 15:47:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 
 # ── Configuración ──────────────────────────────────────────────────
 st.set_page_config(page_title="Dashboard ML", page_icon="", layout="wide")
-st.title("Dashboard de Análisis de Datos Elihu Gay")
+st.title("Dashboard de Análisis de Datos ")
 #st.markdown("Sube un archivo CSV y obtén estadísticas, gráficas y predicciones automáticas.")
 st.divider()
 
